936a93ea2565418acb0273f2755929fed8ce53a8
GowriSakthivel <gowrisp@gmail.com>
2021-01-11 12:12:06 +0530

    Basic web page

---
+++ src/main/java/com/strio/receipeapp/ReceipeappApplication.java
@@ -0,0 +1,13 @@
+package com.strio.receipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceipeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strio/receipeapp/ReceipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strio.receipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
