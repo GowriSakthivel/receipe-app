5b6222acc9b1530fae242454453cc752be7f93f6
GowriSakthivel <gowrisp@gmail.com>
2021-01-11 12:35:50 +0530

    Completed Assignment

---
+++ src/main/java/com/strio/receipeapp/model/Recipe.java
@@ -0,0 +1,116 @@
+package com.strio.receipeapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private int prepTime;
+    private int cookInt;
+    private int serving;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToMany(cascade = CascadeType.ALL , mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(int prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public int getCookInt() {
+        return cookInt;
+    }
+
+    public void setCookInt(int cookInt) {
+        this.cookInt = cookInt;
+    }
+
+    public int getServing() {
+        return serving;
+    }
+
+    public void setServing(int serving) {
+        this.serving = serving;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+}
